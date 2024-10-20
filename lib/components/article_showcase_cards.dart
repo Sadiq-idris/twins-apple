e462928721f79b9afa9761577c12fca56234f055
Sadiq-idris <sadiqidris888@gmail.com>
2024-10-20 04:53:59 +0100

    article showcase card update

--- lib/components/article_showcase_cards.dart
+++ lib/components/article_showcase_cards.dart
@@ -18,41 +18,43 @@ class _ArticleShowcaseCardsState extends State<ArticleShowcaseCards> {
       stream: _cloud.read(),
       builder: (context, snapshot) {
         if (snapshot.hasData) {
-          final documents = snapshot.data!.docs.getRange(0, 3).toList();
+          final response = snapshot.data!.docs;
+          if (response.isNotEmpty) {
+            final documents = response.getRange(0, 3).toList();
 
-          return ListView.builder(
-              primary: false,
-              shrinkWrap: true,
-              itemCount: documents.length,
-              itemBuilder: (context, index) {
-                // getting the data
-                final data = documents[index].data();
-                Map<String, dynamic> article = data;
-          
-                // getting the id for each document
-                String docId = documents[index].id;
-          
-                // converting timestamp to datetime
-                DateTime dateTime = article["createAt"].toDate();
-          
-                // formatting the datetime
-                String formattedDate =
-                    DateFormat("yMMMMEEEEd").format(dateTime);
-          
-                return ArticleTile(
-                  title: article["title"],
-                  imageUrl: article["thumbnailUrl"],
-                  createAt: formattedDate,
-                  docId: docId,
-                  content: article["content"],
-                );
-              });
-        } else if (snapshot.connectionState == ConnectionState.waiting) {
-          return const Center(
-            child: CircularProgressIndicator(),
-          );
+            return ListView.builder(
+                primary: false,
+                shrinkWrap: true,
+                itemCount: documents.length,
+                itemBuilder: (context, index) {
+                  // getting the data
+                  final data = documents[index].data();
+                  Map<String, dynamic> article = data;
+
+                  // getting the id for each document
+                  String docId = documents[index].id;
+
+                  // converting timestamp to datetime
+                  DateTime dateTime = article["createAt"].toDate();
+
+                  // formatting the datetime
+                  String formattedDate =
+                      DateFormat("yMMMMEEEEd").format(dateTime);
+
+                  return ArticleTile(
+                    title: article["title"],
+                    imageUrl: article["thumbnailUrl"],
+                    createAt: formattedDate,
+                    docId: docId,
+                    content: article["content"],
+                  );
+                });
+          } else if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
         }
-
         return Text("Something is wrong");
       },
     );
